0c834d2a166ecc20f5008eb18ce5776f84c05065
Michael Washburn <washburn.michael@gmail.com>
2016-02-19 09:49:32 -0800

    reduced tile size to get three across

--- app/assets/javascripts/contentController.js
+++ app/assets/javascripts/contentController.js
@@ -5,9 +5,6 @@ ContentController.$inject = ['$http'];
 function ContentController($http){
   var vm = this;
 
-  vm.test = 'This is only a test...';
-  vm.allContents = true;
-
   vm.getData = function(){
     $http.get('http://localhost:3000/api/contents')
       .success(function(data) {
